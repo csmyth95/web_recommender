63785e33e01a3dd3af886764ee9eb20ebd181450
csmyth <csmyth95@gmail.com>
2017-03-27 10:36:41 +0100

    Create clusters before running HTTP server

--- web_recommender/http_server.py
+++ web_recommender/http_server.py
@@ -7,6 +7,9 @@ import sys
 from time import time
 import parse_html
 
+# Global variables
+FILE_PATH='~/Library/Application Support/Google/Chrome/Default'
+
 
 class HandleHTTP(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -47,8 +50,8 @@ class HandleHTTP(BaseHTTPRequestHandler):
         return
 
 
-def getRecommendedLinks(links):
-    """Utilises clusters to recommend links from a list of links"""
+# def getRecommendedLinks(links):
+#     """Utilises clusters to recommend links from a list of links"""
 
 
 def run(server_class=HTTPServer, handler_class=HandleHTTP, port=9000):
@@ -68,5 +71,81 @@ if __name__ == "__main__":
         run(port=int(sys.argv[1]))
     else:
         # TODO: Start collecting history from here
+        cwd = os.getcwd()
+
+    	parser = argparse.ArgumentParser()
+    	parser.add_argument(
+    		'--file-path',
+    		'-f',
+    		default=FILE_PATH,
+    		help='path to History sqlite file of Chrome.'
+    	)
+    	parser.add_argument(
+    		'--chrome-path',
+    		default='open -a /Applications/Google\ Chrome.app %s',
+    		help='set chrome path for the specific OS. Default=%(default)s'
+    	)
+    	parser.add_argument(
+    		'--current-dir',
+    		default=cwd,
+    		help='The current working directory where this script is being run.'
+    	)
+    	parser.add_argument(
+    		'--chrome-url',
+    		default='https://www.google.ie/',
+    		help='URL to open after script is run'
+    	)
+        parser.add_argument(
+    		'--url-limit',
+    		default=200,
+    		help='Set limit for the amount of URLs to parse. Default=%(default)s'
+    	)
+        parser.add_argument(
+            'true-k',
+            default=10,
+            help='Number of clusers to create from the user\'s history'
+        )
+        parser.add_argument(
+                "--lsa",
+                dest="n_components", type="int",
+                help="Preprocess documents with latent semantic analysis."
+        )
+        parser.add_argument(
+            "--no-minibatch",
+            action="store_false",
+            dest="minibatch",
+            default=True,
+            help="Use ordinary k-means algorithm (in batch mode)."
+        )
+        parser.add_argument(
+            "--use-idf",
+            action="store_false",
+            default=True,
+            help="Disable Inverse Document Frequency feature weighting."
+        )
+        parser.add_argument(
+            "--use-hashing",
+            action="store_true",
+            default=False,
+            help="Use a hashing feature vectorizer"
+        )
+        parser.add_argument(
+            "--n-features",
+            type=int,
+            default=10000,
+            help="Maximum number of features (dimensions) to extract from text.")
+        parser.add_argument(
+            "--verbose",
+            action="store_true",
+            default=False,
+            help="logging.info progress reports inside k-means algorithm."
+        )
+    	args = parser.parse_args()
+    	# TODO: add some parameter to switch between different functionality of the script
+    	get_history.copy_chrome_history(args.file_path, args.current_dir)
+        urls = parse_html.get_urls(args.current_dir)
+        text_docs = parse_html.parse_html(urls, arguments.url_limit)
+        doc_clusters, doc_cluster_terms = cluster_docs(text_docs, opts, args.true_k)
         logging.info("INFO: History collected, parsed and ready for recommendations.")
+        get_history.open_chrome(args.chrome_path, args.chrome_url)
         run()
