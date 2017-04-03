7bd631c787aef979a43d6c8fcdd3d9c306b3ec63
csmyth <csmyth95@gmail.com>
2017-04-03 13:48:19 +0100

    Archive sqlite3 db functions, error with insertions

--- web_recommender/get_history.py
+++ web_recommender/get_history.py
@@ -11,15 +11,18 @@ import os
 import webbrowser
 import sys
 import re
-import subprocess
 import sqlite3
 import shutil
+
+# Local imports
+import parse_html
 
 logging.basicConfig(level=logging.INFO)
 
 # global variables
 FILE_PATH='~/Library/Application Support/Google/Chrome/Default'
 TEXT_FILE='history.txt'
+TABLE_NAME = "parsed_urls"
 
 
 def copy_chrome_history(path_to_file, current_dir):
@@ -56,53 +59,80 @@ def copy_chrome_history(path_to_file, current_dir):
 		raise SystemExit(1)
 
 
-# def send_to_db(current_dir, doc_clusters, doc_cluster_terms):
+# def send_to_db(urls_with_text):
 #  	"""Send URLs and text obtained from Chrome History to sqlite3 database.
 #
-# 	TODO:
-#  	- Ensure data is not duplicated
-# 	- Entries need a clear row definition
+# 	Parameters:
+# 	- urls_with_text: dictionary of the form {url: "text document"}
 # 	"""
 #
-#  	filename = current_dir+'/'+TEXT_FILE
-#  	# TODO: Tidy up to work with history.txt
 # 	try:
 # 		connection = sqlite3.connect('History')
 # 		logging.info('Database connected to successfully')
-# 		# TODO: Limit number of urls sent to database e.g last - 100
-# 		# if table doesn't exist:
-# 		# 	# Create table
-# 		# 	logging.info("INFO: Creating cluster table")
-# 		# 	connection.execute('''CREATE TABLE COMPANY
-# 		# 		(ID INT PRIMARY KEY     NOT NULL,
-# 		# 		NAME           TEXT    NOT NULL,
-# 		# 		SIZE            INT     NOT NULL,
-# 		# 		ADDRESS        CHAR(50),
-# 		# 		SALARY         REAL);'''
-# 		# 	)
+# 		# if table exists, drop it to store only the latest user browsing history
+# 		logging.info("Dropping table if it exists already...")
+# 		drop_table = "drop table if exists %s" % (TABLE_NAME)
+# 		connection.execute(drop_table)
+# 		# Create table
+# 		logging.info("Creating table to store urls and parsed text from History.")
+# 		connection.execute('''CREATE TABLE %s(
+# 			ID		    INT PRIMARY KEY     NOT NULL,
+# 			URL 	    TEXT    NOT NULL,
+# 			DOCUMENT	TEXT     NOT NULL);''' % (TABLE_NAME)
+# 		)
 #
-# 		for cluster in doc_clusters:
-# 			connection.execute("INSERT INTO ")
-# 		# #connection.execute("SELECT url FROM urls ORDER BY last_visit_time ASC")
-# 		# with open(history_file, 'w') as f:
-# 		# 	for row in cursor:
-# 		# 		f.write(row[0]+'\n')
-# 		# logging.info('History database has been copied to: '+history_file)
+# 		counter = 0
+# 		logging.info("Table created, insert user history data")
+# 		# TODO: Fix insertion error
+# 		cursor = connection.cursor()
+# 		for url in urls_with_text:
+# 			statement = ('INSERT INTO %s (ID, URL, DOCUMENT) '
+# 			'VALUES (%d, "%s", "%s")') % (TABLE_NAME, counter, url, urls_with_text[url])
+# 			logging.info("Statement for insertion: "+statement)
+# 			cursor.execute(statement)
+# 			counter += 1
+# 		logging.info("Values ready to be inserted, commit the transaction")
+# 		connection.commit()
 # 		connection.close()
 # 		logging.info('sqlite3 connection closed.')
 # 	except sqlite3.Error as e:
 # 		logging.error('Sqlite3 error: '+str(e))
-
-
-def open_chrome(chrome_path, url=None):
-	logging.info('Opening Chrome Browser')
-	if sys.platform.startswith('darwin'):
-		chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-	if sys.platform.startswith('win32'):
-		chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
-	if sys.platform.startswith('linux'):
-		chrome_path = '/usr/bin/google-chrome %s'
-	webbrowser.get(chrome_path).open(url)
+# 		logging.error(sys.exc_info())
+# 		sys.exit(1)
+#
+#
+# def retrieve_from_db():
+# 	"""Retrieve stored urls and text documents from local SQLite database
+#
+# 	Returns dictionary of the form: {url: "text document"}
+# 	"""
+# 	urls_dict = dict()
+# 	try:
+# 		connection = sqlite3.connect('History')
+# 		logging.info('Database connected to successfully')
+# 		cursor = connection.execute("SELECT url, document FROM if exists %s ORDER BY id ASC" % TABLE_NAME)
+# 		for row in cursor:
+# 			urls_dict[row[1]] = row[2]
+# 		logging.info('Database entries retrieved from: %s' % TABLE_NAME)
+# 		logging.info(urls_dict)
+# 		connection.close()
+# 		logging.info('sqlite3 connection closed.')
+# 	except sqlite3.Error as e:
+# 		logging.error('Sqlite3 error: '+str(e))
+# 		sys.exit(1)
+# 	sys.exit(1)
+# 	return urls_dict
+#
+#
+# def open_chrome(chrome_path, url=None):
+# 	logging.info('Opening Chrome Browser')
+# 	if sys.platform.startswith('darwin'):
+# 		chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+# 	if sys.platform.startswith('win32'):
+# 		chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
+# 	if sys.platform.startswith('linux'):
+# 		chrome_path = '/usr/bin/google-chrome %s'
+# 	webbrowser.get(chrome_path).open(url)
 
 
 def clean_up():
@@ -136,10 +166,17 @@ def main():
 		default='https://www.google.ie/',
 		help='URL to open after script is run'
 	)
+	parser.add_argument(
+		'--url-limit',
+		default=200,
+		help='Set limit for the amount of URLs to parse. Default=%(default)s'
+	)
 	args = parser.parse_args()
-	# TODO: add some parameter to switch between different functionality of the script
 	copy_chrome_history(args.file_path, args.current_dir)
-	#open_chrome(args.chrome_path, args.chrome_url)
+	urls = parse_html.get_urls(args.current_dir)
+	text_docs = parse_html.parse_html(urls, args.url_limit)
+	send_to_db(text_docs)
+	urls_with_text = retrieve_from_db()
 
 
 if __name__ == '__main__':
